f4930a9bb6524b95520f3654b87f05db3d252c2b
Nils Hartmann <nils@nilshartmann.net>
2024-11-13 11:37:58 +0100

    Initial

--- workspace/app/recipes/page.tsx
+++ workspace/app/recipes/page.tsx
@@ -1,3 +1,8 @@
+import { fetchRecipes } from "@/app/components/queries.ts";
+import RecipeListNavBar from "@/app/components/recipelistpage/RecipeListNavBar.tsx";
+import RecipeListPaginationBar from "@/app/components/recipelistpage/RecipeListPaginationBar.tsx";
+import { RecipeList } from "@/app/components/recipelistpage/RecipeList.tsx";
+
 // todo:
 //  1. "Controller-Funktionalität":
 //     - Recipes laden mit fetchRecipes()
@@ -6,7 +11,11 @@
 //  2. Wo wird gerendert, wie sieht HTML aus?
 //     - console.log("RENDERED AT", new Date().toISOString());
 //
-//  3. Was passiert beim Seitenwechsel?
+//  3. What happens during build time?
+//
+//    export const dynamic = "force-dynamic";
+//
+//  4. Was passiert beim Seitenwechsel?
 //
 //     type RecipesPageProps = {   searchParams: Record<string, string>; };
 //
@@ -14,7 +23,10 @@
 //     - <RecipeListPaginationBar pageable={recipes} params={searchParams} />
 //     - "Blättern" vs. "Open in New Tab"
 //     - JAVASCRIPT AUSSCHALTEN!
+//
+//  5. What happens during build time?
+//    ENTFERNEN: export const dynamic = "force-dynamic";
 
-export default async function RecipeListPage() {
+export default function RecipeListPage() {
   return <div className={"container mx-auto"}>todo</div>;
 }

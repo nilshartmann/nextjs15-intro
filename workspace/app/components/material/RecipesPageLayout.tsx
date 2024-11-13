e4c8290caa8cb5a59fde3ecc3bdf2da4940c1202
Nils Hartmann <nils@nilshartmann.net>
2024-11-13 11:45:19 +0100

    Layout with timer

--- workspace/app/components/material/RecipesPageLayout.tsx
+++ workspace/app/components/material/RecipesPageLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import RecipesHeader from "../layout/RecipesHeader.tsx";
+import { Timer } from "@/app/components/Timer.tsx";
 
 type RecipesPageLayoutProps = {
   children: ReactNode;
@@ -11,8 +12,7 @@ export function RecipesPageLayout({ children }: RecipesPageLayoutProps) {
   //  - was passiert beim Seitenwechsel?
   return (
     <>
-      <RecipesHeader />
-      {/*<RecipesHeader>{<Timer />}</RecipesHeader>*/}
+      <RecipesHeader>{<Timer />}</RecipesHeader>
       <main className={"flex-grow"}>{children}</main>
     </>
   );
